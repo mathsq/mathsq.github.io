86d502f3e39e4952d10a2db874b530a9d2f4d199
srobik <jakub.sliacan@gmail.com>
2016-10-05 13:56:10 +0100

    test collapse calendar

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2376,10 +2376,3 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-
-$('.row .btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $collapse = $this.closest('.collapse-group').find('.collapse');
-    $collapse.collapse('toggle');
-});
